34223321e93f597afd14720004b862908fac7f35
Dev621-tech <devinosborne11@yahoo.com>
2026-02-27 16:52:17 -0500

    Setup getPokemon() Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import Form from './Components/Form.jsx';
 import PokemonDisplay from './Components/PokemonDisplay.jsx';
+import { useState, useEffect } from "react";
 import './App.css'
 
 
 
 function App() {
 
+  const [ pokemon, setPokemon ] = useState(null);
+
+  const getPokemon = async(searchTerm) => {
+    try {
+      
+    } catch (error) {
+      console.error(error)
+    }
+  };
 
   return (
     <>
