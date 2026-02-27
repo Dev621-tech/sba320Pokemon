136aba2b8ce041c49f852d0a266ac91ebc95bb08
Dev621-tech <devinosborne11@yahoo.com>
2026-02-27 17:42:07 -0500

    UpperCased Name

--- src/Components/PokemonDisplay.jsx
+++ src/Components/PokemonDisplay.jsx
@@ -3,7 +3,7 @@ export default function PokemonDisplay({ pokemon }){
     const loaded = () => {
         return (
             <>
-            <h1>{pokemon.species.name}</h1>
+            <h1>{pokemon.species.name.charAt(0).toUpperCase() + pokemon.species.name.slice(1)}</h1> 
             <img src={pokemon.sprites.front_default} alt={pokemon} />
             </>
         );
