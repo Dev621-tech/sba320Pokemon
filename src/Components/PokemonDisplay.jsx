d4a36da593519b6986dabe13d5fa8cd3a1b94022
Dev621-tech <devinosborne11@yahoo.com>
2026-03-02 15:44:41 -0500

    Pokemon Types Map

--- src/Components/PokemonDisplay.jsx
+++ src/Components/PokemonDisplay.jsx
@@ -4,10 +4,20 @@ export default function PokemonDisplay({ pokemon }){
         return (
             <>
             <h1>{pokemon.species.name.charAt(0).toUpperCase() + pokemon.species.name.slice(1)}</h1> 
+            
+              <div>
+                {pokemon.types.map(( type, index ) => (
+                     <div key={index} className={`typeBadge ${type.type.name}`}>
+                            {type.type.name}
+                            </div>
+                ))}
+            </div>
             <img src={pokemon.sprites.front_default} alt={"pokemon"} />
             <img src={pokemon.sprites.back_default} alt={"bpokemon"} />
             <img src={pokemon.sprites.front_shiny} alt={"shiny"} />
             <img src={pokemon.sprites.back_shiny} alt={"bshiny"} />
+
+          
 
             <div>
                 {pokemon.moves.map((move, index) => {
