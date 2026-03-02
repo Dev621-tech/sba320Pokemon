8d0ce3be6bc4c1304f053e6b17e4156d9e37c7bd
Dev621-tech <devinosborne11@yahoo.com>
2026-03-02 15:58:07 -0500

    Capitalize Types + The Rest

--- src/Components/PokemonDisplay.jsx
+++ src/Components/PokemonDisplay.jsx
@@ -1,33 +1,41 @@
-export default function PokemonDisplay({ pokemon }){
-    
+export default function PokemonDisplay({ pokemon }) {
+
     const loaded = () => {
         return (
             <>
-            <h1>{pokemon.species.name.charAt(0).toUpperCase() + pokemon.species.name.slice(1)}</h1> 
-            
-              <div>
-                {pokemon.types.map(( type, index ) => (
-                     <div key={index} className={`typeBadge ${type.type.name}`}>
-                            {type.type.name}
-                            </div>
-                ))}
-            </div>
-            <img src={pokemon.sprites.front_default} alt={"pokemon"} />
-            <img src={pokemon.sprites.back_default} alt={"bpokemon"} />
-            <img src={pokemon.sprites.front_shiny} alt={"shiny"} />
-            <img src={pokemon.sprites.back_shiny} alt={"bshiny"} />
+                <h1>
+                    {pokemon.species.name.charAt(0).toUpperCase() + pokemon.species.name.slice(1)}
+                </h1>
 
-          
-
-            <div>
-                {pokemon.moves.map((move, index) => {
-                    return (
-                        <div key={index} className="move">
-                            {move.move.name}
+                <div>
+                    {pokemon.types.map((type, index) => (
+                        <div key={index} className={`typeBadge ${type.type.name}`}>
+                            {type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)}
                         </div>
-                    )
-                })}
-            </div>
+                    ))}
+                </div>
+                <div>
+                    <h2>Normal</h2>
+                    <img src={pokemon.sprites.front_default} alt={"pokemon"} />
+                    <img src={pokemon.sprites.back_default} alt={"bpokemon"} />
+                </div>
+                <div>
+                    <h2>Shiny</h2>
+                    <img src={pokemon.sprites.front_shiny} alt={"shiny"} />
+                    <img src={pokemon.sprites.back_shiny} alt={"bshiny"} />
+                </div>
+
+
+
+                <div>
+                    {pokemon.moves.map((move, index) => {
+                        return (
+                            <div key={index} className="move">
+                                {move.move.name}
+                            </div>
+                        )
+                    })}
+                </div>
             </>
 
         );
